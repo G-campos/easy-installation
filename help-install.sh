86882913db36fa9cac003eba3eddf26d446a3a93
Gabriel de Campos <gabodecampos@gmail.com>
2019-05-21 20:07:16 -0300

    Update help-install.sh

--- help-install.sh
+++ help-install.sh
@@ -7,6 +7,40 @@
 #Script developed from examples found in the forum of viva la linux
 #
 ####################################################################
+function update(){
+	echo "Updating repositories..."
+	if ! sudo apt-get update ; then
+		echo "Unable to update repositories. Check your file /etc/apt/sources.list"
+		exit 1
+	fi
+	echo "Repositories successfully updated"
+
+	echo "Updating already installed packages..."
+	if ! sudo apt-get upgrade -y ; then
+		echo "Could not update packages."
+		echo 1		
+	fi
+	echo "Packages successfully updated"
+}
+function upsys(){
+	if [[ condition ]]; then
+		#statements
+	fi
+}
+
+
+
+
+
+
+
+
+
+echo "Do you want to update before you start?
+Yes(y) or Not(any key)"
+echo "->Option:  "
+read funtion upsys()
+
 
 echo "==========================================================
         Enter the ID of the component to be installed:
@@ -25,15 +59,14 @@ echo "==========================================================
             11  -   POSTMAN         SNAP
             12  -   RUBYMINE        SNAP
             13  -   SPOTIFY         SNAP
-            14  -   STEAM           DOWNLOAD
-            15  -   SUBLIME TEXT    SNAP
-            16  -   TEAM VIEWER     DOWNLOAD
-            17  -   TELEGRAM        SNAP
-            18  -   TIMESHIFT       APT
-            19  -   VIRTUAL BOX     DOWNLOAD
-            20  -   VS CODE         SNAP
-            21  -   VLC             SNAP
-            22  -   WPS OFFICE      SNAP
+            14  -   SUBLIME TEXT    SNAP
+            15  -   TEAM VIEWER     DOWNLOAD
+            16  -   TELEGRAM        SNAP
+            17  -   TIMESHIFT       APT
+            18  -   VIRTUAL BOX     DOWNLOAD
+            19  -   VS CODE         SNAP
+            20  -   VLC             SNAP
+            21  -   WPS OFFICE      SNAP
             0   -   SAIR
 
 
