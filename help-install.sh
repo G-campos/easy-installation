c273a93b2f95f93182ac430f639dc35b6c0b1fed
Gabriel de Campos <gabodecampos@gmail.com>
2019-05-21 19:08:17 -0300

    Update help-install.sh

--- help-install.sh
+++ help-install.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 #help-install.sh
+#developed by Campos, distributed the Kontrol Bits System
+#origin: https://github.com/G-campos/easy-installation
+#I hope the feedback =)
 #
+#Script developed from examples found in the forum of viva la linux
 #
-#
-#
-#
+####################################################################
+
+echo "==========================================================
+        Digite o número do componente a ser instalado:
+            ID  -   PROGRAMA        ORIGEM
+            
+            1   -   GDEBI           APT
+            2   -   GIMP            SNAP
+            3   -   GOOGLE          DOWNLOAD
+            4   -   JAVA            DOWNLOAD
+            5   -   JAVA FX         DOWNLOAD
+            6   -   JAVA SDK        DOWNLOAD
+            7   -   KOLOURPAINT     SNAP
+            8   -   NETBEANS        SNAP
+            9   -   NODEJS          SNAP
+            10  -   PHPSTORM        SNAP
+            11  -   POSTMAN         SNAP
+            12  -   RUBYMINE        SNAP
+            13  -   SPOTIFY         SNAP
+            14  -   STEAM           DOWNLOAD
+            15  -   SUBLIME TEXT    SNAP
+            16  -   TEAM VIEWER     DOWNLOAD
+            17  -   TELEGRAM        SNAP
+            18  -   TIMESHIFT       APT
+            19  -   VIRTUAL BOX     DOWNLOAD
+            20  -   VS CODE         SNAP
+            21  -   VLC             SNAP
+            22  -   WPS OFFICE      SNAP
+            0   -   SAIR
+
+
+==========================================================="
